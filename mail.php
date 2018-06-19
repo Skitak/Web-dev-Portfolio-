fb09b8d0259d70301b41a854f29170ecec4dc7a0
skitak <bas2205@live.fr>
2018-06-19 08:59:42 +0200

    mail send to both corectly

--- mail.php
+++ mail.php
@@ -17,7 +17,7 @@ else {
     $msg += $_POST["message"];
 
     // use wordwrap() if lines are longer than 70 characters
-    $msg = wordwrap($msg,70);
+    // $msg = wordwrap($msg,70);
 
     $headers = "From: " . $_POST["nom"] . " <" . $_POST["email"] . ">";
     
